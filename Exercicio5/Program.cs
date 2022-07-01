3b2a0c0dd013e8648ef29a4897ab0725a21f5945
Marcelo Jesus <marchello0234@gmail.com>
2022-07-01 14:48:02 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Fazer um programa para encontrar todos os números pares entre 1 e 100.
+
+            
+
+            for(int i = 0; i <= 101; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine("Número par: " + i + "\t");
+                }  
+
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
